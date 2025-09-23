4e93312d22a78066774e28f77050ab30df536001
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-09-23 21:29:28 +0600

    Fix

--- scripts/cmds/edit1.js
+++ scripts/cmds/edit1.js
@@ -1,35 +1,26 @@
 const axios = require('axios');
-
 module.exports = {
-  config: {
-    name: "edit1",
-    author: "frnwot",
-    category: "media",
-    countDown: 5,
-    role: 0,
-    guide: { en: "edit1 <prompt>" }
-  },
-
-  onStart: async function({ message, event, args }) {
-    const prompt = args.join(" ");
-    if (!prompt) {
-      return message.reply("❌ | provide a prompt");
-    }
-
-    message.reaction("⏳", event.messageID);
-
-    try {
-      let url = `https://aryan-nix-apis.vercel.app/api/gemini?prompt=${encodeURIComponent(prompt)}`;
-      const res = await axios.get(url);
-
-      await message.reply({
-        body: res.data?.response || "❌ | No response received."
-      });
-
-      message.reaction("✅", event.messageID);
-    } catch (error) {
-      message.reaction("❌", event.messageID);
-      message.reply("❌ | " + error.message);
-    }
-  }
-};
+config: {
+	name: "edit1",
+	author: "Tawsif based",
+	category: "media",
+	countDown: 5,
+	role: 0,
+	guide: { en: "edit <prompt> | reply to image"
+}
+},
+onStart: async function({ message, event, args }) {
+const prompt = args.join(" ");
+if (!event.messageReply || !event?.messageReply?.attachments[0]?.url) { return message.reply('reply to an image');
+} else if (!prompt) { return message.reply("❌ | provide a prompt");
+}
+const replyImageUrl = event.messageReply.attachments[0].url;	message.reaction("⏳", event.messageID);
+try {
+		let url = `https://tawsifs-gemini.onrender.com/nano?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(replyImageUrl)}`;
+const res = await axios.get(url);
+await message.reply({ attachment: await global.utils.getStreamFromURL(res.data.data.imageResponseVo.urls[0], 'edit.png'), body: `✅ | nigga image Edited succesfully`,
+});
+} catch (error) { message.send("❌ | " + error.message);
+		}
+	}
+}
