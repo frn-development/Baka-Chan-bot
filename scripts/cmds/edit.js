6091af42f69dfa1d75f93fc64dcd8ac47dae50fc
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-10-01 21:07:55 +0600

    Updated

--- scripts/cmds/edit.js
+++ scripts/cmds/edit.js
@@ -1,11 +1,13 @@
+const { NanoBanana: gen } = require('/tools/ai-image.js');
 const axios = require('axios');
 module.exports = {
 config: {
 	name: "edit",
 	author: "Tawsif~",
-	category: "media",
+	category: "ai",
 	countDown: 5,
 	role: 0,
+description: "edit images using Nano-Banana AI",
 	guide: { en: "edit <prompt> | reply to image"
 }
 },
@@ -14,11 +16,11 @@ const prompt = args.join(" ");
 if (!event.messageReply || !event?.messageReply?.attachments[0]?.url) { return message.reply('reply to an image');
 } else if (!prompt) { return message.reply("❌ | provide a prompt");
 }
-const replyImageUrl = event.messageReply.attachments[0].url;	message.reaction("⏳", event.messageID);
+const imageUrl = event.messageReply.attachments[0].url;
+	message.reaction("⏳", event.messageID);
 try {
-		let url = `https://tawsifs-gemini.onrender.com/nano?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(replyImageUrl)}`;
-const res = await axios.get(url);
-await message.reply({ attachment: await global.utils.getStreamFromURL(res.data.data.imageResponseVo.urls[0], 'edit.png'), body: `✅ | nigga image Edited succesfully`,
+const res = await gen(prompt, imageUrl);
+await message.reply({ attachment: await global.utils.getStreamFromURL(res.url, 'edit.png'), body: `✅ | image Edited succesfully`,
 });
 } catch (error) { message.send("❌ | " + error.message);
 		}
