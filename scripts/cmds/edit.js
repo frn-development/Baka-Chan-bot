f10038f333de0304a4a5e454213af277d8148bf9
Gtajisan <ffjisan804@gmail.com>
2025-10-04 15:05:18 +0600

    added some cmd
    Signed-off-by: Gtajisan <ffjisan804@gmail.com>

--- scripts/cmds/edit.js
+++ scripts/cmds/edit.js
@@ -1,8 +1,7 @@
-const { NanoBanana: gen } = require('/tools/ai-image.js');
 const axios = require('axios');
 module.exports = {
 config: {
-	name: "xdit",
+	name: "edit",
 	author: "Tawsif~",
 	category: "ai",
 	countDown: 5,
@@ -19,8 +18,8 @@ if (!event.messageReply || !event?.messageReply?.attachments[0]?.url) { return m
 const imageUrl = event.messageReply.attachments[0].url;
 	message.reaction("⏳", event.messageID);
 try {
-const res = await gen(prompt, imageUrl);
-await message.reply({ attachment: await global.utils.getStreamFromURL(res.url, 'edit.png'), body: `✅ | image Edited succesfully`,
+const res = await axios.get(`https://tawsif.is-a.dev/gemini/nano-banana?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(imageUrl)}`);
+message.reply({ attachment: await global.utils.getStreamFromURL(res.data.imageUrl, 'edit.png'), body: `✅ | image Edited succesfully`,
 });
 } catch (error) { message.send("❌ | " + error.message);
 		}
